3c6655839cb092340a1d1d361fef1f7c95516fb5
Bharat <bharat.chaudhary2004@gmail.com>
2025-05-18 21:17:39 +0530

    leaning js

---
+++ html_css_js_54/script.js
@@ -0,0 +1,14 @@
+alert("Hello");  // Shows a popup alert
+console.log("Running");  // Print on console
+
+var a = prompt("Enter your number");  // Ask the user to input a number using dialog box
+var t = confirm("Y or N");  // Show a confirm dialog with OK (true) and Cancel (false)
+
+if(t){
+    console.log("Yes sir");
+}
+else{
+    console.log("Nope");
+}
+
+console.log("ur number is " + a);
